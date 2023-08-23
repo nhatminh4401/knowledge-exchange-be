c916124ebdf40b5ff07a15e80db5dc499f7641ea
nhatminh4401 <58182906+nhatminh4401@users.noreply.github.com>
2023-08-23 18:11:52 +0700

    fix small bugs in create user (user-service)

--- user-service/user/views.py
+++ user-service/user/views.py
@@ -34,6 +34,7 @@ class userApi(APIView):
         data = request.data
 
         # Kiểm tra xem người dùng đã tồn tại chưa
+        id = data.get('id')
         username = data.get('username')
         email = data.get('email')
         phone = data.get('phone')
@@ -41,7 +42,7 @@ class userApi(APIView):
         if User.objects.filter(username=username).exists():
             return Response(data={"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = User(username=username, email=email, phone=phone)
+        user = User(id=id, username=username, email=email, phone=phone)
         user.save()
 
         return Response(data={"message": "User created successfully"}, status=status.HTTP_201_CREATED)
